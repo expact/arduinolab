05a699c36c1ffcf3fbcf30669ec19adeca49acf5
Johan Nilsson <github@markupartist.com>
2013-03-05 19:03:59 +0100

    Add resistor, and simplify example by removing const.

--- lab_led01/lab_led01.ino
+++ lab_led01/lab_led01.ino
@@ -1,14 +1,22 @@
-// LED Blink
+/* 
+Making a LED Blink by periodically turning it on and off.
 
-const int ledPin = 13; // The pin on the Arduino.
+Circuit:
+* Connect LED to pin 13 through a resistor. (long leg on the LED)
+* Connect GND to the other leg.
+*/
+
+int ledPin = 13; // The pin on the Arduino.
 
 void setup() {
+  // Code here is run once.
   pinMode(ledPin, OUTPUT); // Configures this pin to be used as output.
-}
+} // End of setup.
 
 void loop() {
+  // Code put here is run repeatedly.
   digitalWrite(ledPin, HIGH); // Turn on LED.
   delay(1000); // Pause this program for 1000 milliseconds (1 second), causes the LED to be turned on for this period. 
   digitalWrite(ledPin, LOW); // Turn off LED.
   delay(1000); // Pause for 1000 milliseconds, ensures the LED is turned off for this period.
-}
+} // End of loop.
